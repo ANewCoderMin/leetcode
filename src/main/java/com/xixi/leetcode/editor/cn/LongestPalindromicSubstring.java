865dd62fa02a277ff2d60817a8786aaf4449817d
王敏 <min.wang@davincimotor.com>
2023-03-06 18:49:11 +0800

    最长回文子串

--- src/main/java/com/xixi/leetcode/editor/cn/LongestPalindromicSubstring.java
+++ src/main/java/com/xixi/leetcode/editor/cn/LongestPalindromicSubstring.java
@@ -3,14 +3,33 @@ package com.xixi.leetcode.editor.cn;
 public class LongestPalindromicSubstring{
     public static void main(){
         Solution solution = new LongestPalindromicSubstring().new Solution();
+        solution.longestPalindrome("babad");
     }
     
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String longestPalindrome(String s) {
-        return null;
+        String result = "";
+        if (s == null || s.isBlank()){
+            return result;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            int[] res = core(i - 1, i + 1, s);
+            if (res[1] - res[0] + 1 > result.length()){
+                result = s.substring(res[0], res[1] + 1);
+            }
+        }
+        return result;
+    }
+
+    public int[] core(int start, int end, String s){
+        while (start >= 0 && end < s.length() && s.indexOf(start) == s.indexOf(end)){
+            start--;
+            end++;
+        }
+        return new int[]{++start, --end};
     }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+
 
 }
