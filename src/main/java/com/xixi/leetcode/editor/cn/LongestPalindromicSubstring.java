895a675bd1aee486bb3d3001d7504ea61647a7d3
王敏 <min.wang@davincimotor.com>
2023-03-07 16:50:00 +0800

    最长回文子串

--- src/main/java/com/xixi/leetcode/editor/cn/LongestPalindromicSubstring.java
+++ src/main/java/com/xixi/leetcode/editor/cn/LongestPalindromicSubstring.java
@@ -1,29 +1,34 @@
 package com.xixi.leetcode.editor.cn;
 
 public class LongestPalindromicSubstring{
-    public static void main(){
+    public static void main(String[] args){
         Solution solution = new LongestPalindromicSubstring().new Solution();
-        solution.longestPalindrome("babad");
+        System.out.println(solution.longestPalindrome("babad"));;
     }
+
     
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public String longestPalindrome(String s) {
-        String result = "";
-        if (s == null || s.isBlank()){
-            return result;
-        }
-        for (int i = 0; i < s.length(); i++) {
-            int[] res = core(i - 1, i + 1, s);
-            if (res[1] - res[0] + 1 > result.length()){
-                result = s.substring(res[0], res[1] + 1);
+    class Solution {
+        public String longestPalindrome(String s) {
+            String result = "";
+            if (s == null || s.isBlank()){
+                return result;
             }
-        }
+            for (int i = 0; i < s.length(); i++) {
+                int[] res = core(i - 1, i + 1, s);
+                if (res[1] - res[0] + 1 > result.length()){
+                    result = s.substring(res[0], res[1] + 1);
+                }
+                res = core(i, i + 1, s);
+                if (res[1] - res[0] + 1 > result.length()){
+                    result = s.substring(res[0], res[1] + 1);
+                }
+            }
         return result;
     }
 
     public int[] core(int start, int end, String s){
-        while (start >= 0 && end < s.length() && s.indexOf(start) == s.indexOf(end)){
+        while (start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)){
             start--;
             end++;
         }
